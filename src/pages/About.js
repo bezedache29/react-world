46de009fc11f022a6aab39489b4e079621cb4f8e
Christophe Salou <bezedache29@gmail.com>
2021-09-23 17:28:38 +0200

    Installation et mise en place du router + pages about - home - 404

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>À propos</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit, fuga voluptatem, consequatur quia in aliquid nam libero animi dolore magnam omnis dolores? Excepturi dolor voluptates aliquid culpa eius praesentium ut possimus ipsam modi maxime vitae ad vero sit mollitia, adipisci, consectetur, nulla iusto corrupti quos. Consequuntur tempore fuga placeat nobis voluptates. Aperiam, cum quo! Itaque modi libero velit molestiae mollitia.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, quaerat repellat atque eveniet magnam id eaque totam est, voluptate quasi sunt nemo optio quae fuga aperiam laboriosam aspernatur ad sapiente et maiores consequatur dolor. Libero nemo quasi autem at eos. Maxime dolorem, magni omnis molestiae fuga in voluptatum deserunt reiciendis voluptatem exercitationem illo vitae? Ea incidunt officia a. Quasi maiores nobis, repudiandae sequi a alias modi autem. Quaerat ducimus nesciunt aperiam nulla magnam impedit possimus, deserunt obcaecati! Exercitationem fugit tenetur quas vel aliquid non debitis. Facilis fuga ipsam nam autem praesentium nisi laborum aliquam consectetur, necessitatibus aperiam tempore ex eveniet soluta harum quo sed perspiciatis qui modi debitis dicta unde tenetur! Quas eos corporis fugiat, nesciunt rerum ea? Nemo architecto ipsam deleniti molestias in porro, facere ab voluptatum sunt delectus recusandae molestiae ratione harum, tempora cum veritatis magni adipisci dignissimos?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Page Home</h1>
+    </div>
+  )
+}
+
+export default Home
